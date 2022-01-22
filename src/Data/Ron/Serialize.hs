a08201e70c35620cbb39fcae116992468a73df73
d86leader <d86leader@gmail.com>
2022-01-22 13:34:37 +0700

    First attempt at rewriting serializer
    Almost great, but problem with putting spaces where not necessary

--- src/Data/Ron/Serialize.hs
+++ src/Data/Ron/Serialize.hs
@@ -1,56 +1,191 @@
+{-# LANGUAGE RecordWildCards #-}
 module Data.Ron.Serialize
-    ( dumpsCompact
+    ( CommaStyle (..)
+    , SerializeSettins (..)
+    , haskellStyle, rustStyle, compactStyle
+    , dumps
     ) where
 
-import Data.ByteString.Builder (Builder, toLazyByteString, byteString, integerDec, doubleDec, char7)
+import Data.ByteString.Builder (Builder, toLazyByteString, byteString, integerDec, doubleDec, char7, string7)
 import Data.ByteString.Builder.Prim (primBounded, condB, (>$<), (>*<), liftFixedToBounded, word16HexFixed)
 import Data.Char (ord)
+import Data.List (intersperse)
 import Data.Text (Text)
 import Data.Text.Encoding (encodeUtf8, encodeUtf8BuilderEscaped)
 import Data.Word (Word8)
 
-import qualified Data.Map as Map
 import qualified Data.ByteString.Lazy as Lazy
 import qualified Data.ByteString.Builder.Prim as Prim
+import qualified Data.Map.Strict as Map
+import qualified Data.Text as Text
 import qualified Data.Vector as Vector
 
 import Data.Ron.Value
 
 
--- | Dumps a RON 'Value' as a compact string with minimum whitespace, but a lot of commas
---
--- TODO: less commas. And also we need a non-compact dumper
-dumpsCompact :: Value -> Lazy.ByteString
-dumpsCompact = toLazyByteString . go where
-    toListElem x = go x <> char7 ','
-    toMapElem (k, v) = go k <> char7 ':' <> go v <> char7 ','
-    toRecordElem (k, v) = fromText k <> char7 ':' <> go v <> char7 ','
+data CommaStyle
+    = CommaHistoric
+    -- ^ Like in json, comma after value without trailing
+    | CommaTrailing
+    -- ^ Comma after value, including last element
+    | CommaLeading
+    -- ^ Haskell style, comma at line start
+    deriving (Eq, Show)
+
+data SerializeSettins = SerializeSettins
+    { commaStyle :: !CommaStyle
+    , indent :: !Int
+    , singleElementSpecial :: !Bool
+    -- ^ When a compound type only contains one element, this compound value is
+    -- printed on one line
+    , unpackToplevel :: !Bool
+    -- ^ Toplevel struct and list are unpacked to not include the constructor
+    -- and brackets
+    , openBracketOnSameLine :: !Bool
+    -- ^ For compound types, does the bracket go on a new line or stays on the
+    -- same line as the constructor
+    , closeBracketOnSameLine :: !Bool
+    -- ^ For compound types, does the bracket go on a new line or stays on the
+    -- same line as the last element. Some people in haskell like this one as
+    -- True, and I think lispers do as well
+    } deriving (Eq, Show)
+
+haskellStyle, rustStyle, compactStyle :: SerializeSettins
+haskellStyle = SerializeSettins
+    { commaStyle = CommaLeading
+    , indent = 4
+    , singleElementSpecial = True
+    , unpackToplevel = True
+    , openBracketOnSameLine = False
+    , closeBracketOnSameLine = False
+    }
+rustStyle = SerializeSettins
+    { commaStyle = CommaTrailing
+    , indent = 4
+    , singleElementSpecial = True
+    , unpackToplevel = False
+    , openBracketOnSameLine = False
+    , closeBracketOnSameLine = False
+    }
+compactStyle = SerializeSettins
+    { commaStyle = CommaHistoric
+    , indent = 0
+    , singleElementSpecial = True
+    , unpackToplevel = False
+    , openBracketOnSameLine = True
+    , closeBracketOnSameLine = True
+    }
+
+dumps :: SerializeSettins -> Value -> Lazy.ByteString
+dumps SerializeSettins {..} = toLazyByteString . toplevel where
+    deeper !lvl = lvl + indent
+    nl  = if indent == 0 then mempty else char7 '\n'
+    shift lvl = string7 $ replicate lvl ' '
+    bracketOpen lvl c
+        | indent == 0  = char7 c
+        | openBracketOnSameLine  = char7 ' ' <> char7 c <> nl <> shift (deeper lvl)
+            <> if commaStyle == CommaLeading
+                then char7 ' ' <> char7 ' '
+                else mempty
+        | commaStyle == CommaLeading  =
+            nl <> shift (deeper lvl) <> char7 c <> char7 ' '
+        | otherwise  = char7 ' ' <> char7 c <> nl <> shift (deeper lvl)
+    bracketClose lvl c
+        | indent == 0  = char7 c
+        | closeBracketOnSameLine  = char7 ' ' <> char7 c
+        | commaStyle == CommaLeading  = nl <> shift (deeper lvl) <> char7 c
+        | otherwise  = nl <> shift lvl <> char7 c
+    comma lvl = if commaStyle == CommaLeading
+        then nl <> shift lvl <> string7 ", "
+        else char7 ',' <> nl <> shift lvl
+    trailingComma = if commaStyle == CommaTrailing
+        then char7 ','
+        else mempty
+    toplevel = if unpackToplevel
+        then \case
+            -- Those two looks bad with leading comma, not sure what to do.
+            -- Don't want to turn them into trailing/historic comma for
+            -- toplevel only, since I personally always forget to put it after
+            -- the previous element
+            List xs -> listContent 0 xs
+            Record _name xs -> recordContent 0 xs
+            v -> go 0 v
+        else go 0
     --
-    go (Integral x) = integerDec x
-    go (Floating x) = doubleDec x
-    go (Char x) = char7 '\'' <> encodeChar x <> char7 '\''
-    go (String x) = char7 '"' <> encodeString x <> char7 '"'
-    go (List xs)
-        = char7 '['
-       <> mconcat (map toListElem (Vector.toList xs))
-       <> char7 ']'
-    go (Map xs)
-        = char7 '{'
-       <> mconcat (map toMapElem (Map.toList xs))
-       <> char7 '}'
-    go (Unit constructor)
-        | constructor == ""  = byteString "()"
-        | otherwise          = fromText constructor
-    go (Tuple mbName xs)
-        = fromText mbName
-       <> char7 '('
-       <> mconcat (map toListElem (Vector.toList xs))
-       <> char7 ')'
-    go (Record mbName xs)
-        = fromText mbName
-       <> char7 '('
-       <> mconcat (map toRecordElem (Map.toList xs))
-       <> char7 ')'
+    go !lvl = \case
+        Integral x -> integerDec x
+        Floating x -> doubleDec x
+        Char x -> char7 '\'' <> encodeChar x <> char7 '\''
+        String x -> char7 '"' <> encodeString x <> char7 '"'
+        Unit name -> if Text.null name
+            then string7 "()"
+            else fromText name
+        List xs
+            | null xs -> string7 "[]"
+            | singleElementSpecial && isSimple (List xs)
+                -> let !x = Vector.unsafeHead xs
+                   in string7 "[ " <> go lvl x <> string7 " ]"
+            | otherwise -> bracketOpen lvl '[' <> listContent (deeper lvl) xs <> bracketClose lvl ']'
+        Map xs
+            | null xs -> string7 "{}"
+            | singleElementSpecial && isSimple (Map xs)
+                -> let (!k, !v) = head . Map.toList $ xs
+                   in string7 "{ " <> go lvl k <> string7 ": " <> go lvl v <> string7 " }"
+            | otherwise -> bracketOpen lvl '{' <> mapContent (deeper lvl) xs <> bracketClose lvl '}'
+        Tuple mbName xs ->
+            let
+              name = if Text.null mbName then mempty else fromText mbName
+              content = if singleElementSpecial && isSimple (Tuple mbName xs)
+                    then let !x = Vector.unsafeHead xs
+                         in string7 "( " <> go lvl x <> string7 " )"
+                    else bracketOpen lvl '(' <> listContent (deeper lvl) xs <> bracketClose lvl ')'
+            in name <> content
+        Record mbName xs ->
+            let
+              name = if Text.null mbName then mempty else fromText mbName
+              content = if singleElementSpecial && isSimple (Record mbName xs)
+                    then let (!k, !v) = head . Map.toList $ xs
+                         in string7 "( " <> fromText k <> string7": " <> go lvl v <> string7 " )"
+                    else bracketOpen lvl '(' <> recordContent (deeper lvl) xs <> bracketClose lvl ')'
+            in name <> content
+    --
+    listContent lvl = (<> trailingComma) . mconcat . intersperse (comma lvl) . map (go lvl) . Vector.toList
+    --
+    mapContent lvl = (<> trailingComma) . mconcat . intersperse (comma lvl) . map toElem . Map.toList where
+        toElem (k, v) = go lvl k <> string7 ": " <> go lvl v
+    --
+    recordContent lvl = (<> trailingComma) . mconcat . intersperse (comma lvl) . map toElem . Map.toList where
+        toElem (k, v) = fromText k <> string7 ": " <> go lvl v
+
+-- | Can the value be nicely rendered on a single line? True for non-compounds and single-value compounds
+isSimple :: Value -> Bool
+isSimple (Integral _) = True
+isSimple (Floating _) = True
+isSimple (Char _) = True
+isSimple (String _) = True
+isSimple (Unit _) = True
+isSimple (List elems) = case Vector.uncons elems of
+    Nothing -> True
+    Just (x, xs)
+        | null xs   -> isSimple x
+        | otherwise -> False
+isSimple (Map xs)
+    | null xs = True
+    | length xs == 1 = case Map.toList xs of
+        [(k, v)] -> isSimple k && isSimple v
+        _ -> False
+    | otherwise = False
+isSimple (Tuple _ elems) = case Vector.uncons elems of
+    Nothing -> True
+    Just (x, xs)
+        | null xs   -> isSimple x
+        | otherwise -> False
+isSimple (Record _ xs)
+    | null xs = True
+    | length xs == 1 = case Map.toList xs of
+        [(_, v)] -> isSimple v
+        _ -> False
+    | otherwise = False
 
 fromText :: Text -> Builder
 fromText = byteString . encodeUtf8
--- test/Test.hs
+++ test/Test.hs
@@ -2,7 +2,7 @@ module Main where
 
 import Data.ByteString.Lazy (toStrict)
 import Data.Ron.Deserialize (loads)
-import Data.Ron.Serialize (dumpsCompact)
+import Data.Ron.Serialize (dumps, compactStyle)
 import Data.Ron.Value.Internal (similar')
 import Test.Tasty.QuickCheck (testProperty)
 
@@ -13,11 +13,11 @@ import Test.Tasty
 main = defaultMain $ testGroup "properties" [always_decodes, loads_dumps, exampleTests]
 
 always_decodes = testProperty "isRight . loads . dumps $ x" $
-    \value -> case loads . toStrict . dumpsCompact $ value of
+    \value -> case loads . toStrict . dumps compactStyle $ value of
         Left _err -> False
         Right _val -> True
 
 loads_dumps = testProperty "loads . dumps = id" $
-    \value -> case loads . toStrict . dumpsCompact $ value of
+    \value -> case loads . toStrict . dumps compactStyle $ value of
         Left err -> False
         Right value' -> value `similar'` value'
